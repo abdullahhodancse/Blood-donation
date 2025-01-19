02aa1bdd9548b075c3f8580fefbeadcf41dbb3cc
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-01-19 15:17:41 +0600

    Updated code

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -30,7 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:5500",  # Frontend URL
+#     "http://127.0.0.1:8000",  # Frontend URL
+# ]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500",
+#     "http://localhost:3000",
 # ]
 
 
@@ -129,10 +133,8 @@ USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = [ 'http://localhost:8000', 'http://127.0.0.1:8000', ]
+
 
 
 
