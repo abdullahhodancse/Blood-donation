7201ddeb66af1e0c7766407099fc2e15cf89336a
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-01-19 14:33:15 +0600

    Updated code

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'donation',
     'service',
     'users_app',
+    'corsheaders',
   
 ]
 
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
    
 ]
 
@@ -124,6 +126,13 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+]
 
 
 
